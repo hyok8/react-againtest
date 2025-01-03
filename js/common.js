56e7dca7ce677941a0291280fca0da49bfa78152
hyok8 <hyo.k824@gmail.com>
2025-01-03 12:14:55 +0900

    자바스크립트 기본 출력해보기

---
+++ js/common.js
@@ -0,0 +1,8 @@
+const mystudy = "리액트"; //절대 띄어쓰기, 대문자,숫자로 시작하면 안됌!!!
+document.body.innerHTML = "<h1>" + mystudy + "</h1>";
+let myname = "김효정";
+document.body.innerHTML += "<h2>" + myname + "</h2>";
+let myread = "어려워";
+document.body.innerHTML += "<p>" +myread + "</p>";
+let mystudy2 = "리액트";
+document.body.innerHTML += "<h2>" +mystudy2+ "</h2>";
